4eef834a34347e2cade70ee8a6bdea3b99c5ba13
Patrick McCarren <patrick@mmsc.io>
2015-03-19 21:52:23 -0400

    ApiController: Added it

---
+++ src/Http/Controllers/ApiController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace DroneMill\FoundationApi\Http\Controllers;
+
+use BaseController as TheBaseController; // BaseController is already in use
+use EchoIt\JsonApi\Request as ApiRequest;
+use EchoIt\JsonApi\ErrorResponse as ApiErrorResponse;
+use EchoIt\JsonApi\Exception as ApiException;
+use Request;
+
+class ApiController extends TheBaseController
+{
+	public function handleRequest($modelName, $id = null)
+	{
+		/**
+		 * Forumulate an entity name
+		 *
+		 * @var  string
+		 */
+		$entity = ucfirst(str_singular($modelName));
+
+		/**
+		* Create handler name from model name
+		* @var string
+		*/
+		$handlerClass = 'App\\Handlers\\Models\\' . $entity . 'Handler';
+
+
+		// ensure we actually have a handler class
+		if (! class_exists($handlerClass))
+		{
+			// if a handler class does not exist for requested model,
+			// it is not considered to be exposed in the API
+			return new ApiErrorResponse(404, 404, 'Entity [' . $entity . '] not found');
+		}
+
+		// store request input, and params
+		$url        = Request::url();
+		$method     = Request::method();
+		$include    = ($i = Request::input('include')) ? explode(',', $i) : $i;
+		$sort       = ($i = Request::input('sort')) ? explode(',', $i) : $i;
+		$filter     = ($i = Request::except('sort', 'include', 'page')) ? $i : [];
+		$content    = Request::getContent();
+		$page       = Request::input('page');
+		$pageSize   = null;
+		$pageNumber = null;
+
+		// setup pagination
+		if ($page)
+		{
+			// if we are doing pagination, ensure that we received the propper params
+			if (is_array($page) && !empty($page['size']) && !empty($page['number']))
+			{
+				$pageSize = $page['size'];
+				$pageNumber = $page['number'];
+			}
+			else
+			{
+				return new ApiErrorResponse(400, 400, 'Expected page[size] and page[number]');
+			}
+		}
+
+		// instantiate new ApiRequest object
+		$request = new ApiRequest(Request::url(), $method, $id, $content, $include, $sort, $filter, $pageNumber, $pageSize);
+
+		// instantiate new Handler
+		$handler = new $handlerClass($request);
+
+		// a handler can throw EchoIt\JsonApi\Exception which must be gracefully handled to give proper response
+		try
+		{
+			$res = $handler->fulfillRequest();
+		}
+		catch (ApiException $e)
+		{
+			return $e->response();
+		}
+
+		// return the json response
+		return $res->toJsonResponse();
+	}
+}
