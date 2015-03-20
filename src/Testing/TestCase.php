5cf1cf422f8c1e2163b689946f75c4285908ed26
Patrick McCarren <patrick@mmsc.io>
2015-03-19 23:41:04 -0400

    TestCase: Added better documentation

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -8,34 +8,71 @@ use File;
 use Mockery;
 use Clousure;
 
-// TODO: Comment this class
 
+/**
+ * A TestCase Foundation
+ */
 abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 
+	/**
+	 * Our FactoryMuffin instance
+	 *
+	 * @var  League\FactoryMuffin\FactoryMuffin
+	 */
 	protected static $factoryMuffin = null;
 
+	/**
+	 * Track if we have seeded the database or not
+	 *
+	 * @var  boolean
+	 */
 	protected $seeded = false;
+
+	/**
+	 * Track if we have run migrations on the db
+	 *
+	 * @var  boolean
+	 */
 	protected $migrated = false;
 
+	/**
+	 * Bootstrap the testing environment
+	 *
+	 * @return void
+	 */
 	public static function setupBeforeClass()
 	{
+		/**
+		 * If the class 'App' does not exist, then that means that we are
+		 * the very first test class being instantiated, and we need to
+		 * bootstrap the application so that the AliasLoader is registered
+		 */
 		if (! class_exists('App'))
 		{
 			$app = new Static;
 			$app->createApplication();
 		}
 
+		// create our FactoryMuffin instance
 		self::$factoryMuffin = new FactoryMuffin();
+
+		// loud the factory definitions we have declared
 		self::$factoryMuffin->loadFactories(static::FACTORIES_PATH);
 	}
 
+	/**
+	 * This is the setUp method run before each test method
+	 *
+	 * @return void
+	 */
 	public function setUp()
 	{
 		parent::setUp();
 
+		// reset model events
 		static::resetModelEvents();
 
-		// If we do not have a migrated DB, then migrate
+		// if we do not have a migrated DB, then migrate
 		if (! $this->migrated)
 		{
 			$this->migrateUp();
@@ -44,6 +81,12 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 
 	}
 
+	/**
+	 * Seed the db, and track that we did so
+	 *
+	 * @param  string  $class
+	 * @return void
+	 */
 	public function seed($class = 'DatabaseSeeder')
 	{
 		$this->seeded = true;
@@ -68,6 +111,7 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 	{
 		foreach ($pathToModels as $path)
 		{
+			// fetch a listing of all the files in the given model path
 			$files = File::files($path['path']);
 
 			// Remove the directory name and the .php from the filename
@@ -90,20 +134,30 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 		}
 	}
 
+	/**
+	 * Extraneous cleanup routines to be run after each test method
+	 */
 	public function cleanup()
 	{
 
 	}
 
+	/**
+	 * Tear down and cleanup after a test case method
+	 */
 	public function tearDown()
 	{
+		// stop mocking any classes we are currently mocking
 		Mockery::close();
 
+		// delete models that we have saved to the db
+		// note: this happens in the reverse order as they were created
 		self::$factoryMuffin->deleteSaved();
 
+		// run any extraneous cleanup routines we have
 		$this->cleanup();
 
-		// if we seeded the db, then migrate down
+		// if we seeded the db, then migrate down, thus unseeding the db
 		if ($this->seeded)
 		{
 			$this->migrateDown();
@@ -114,11 +168,20 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 		parent::tearDown();
 	}
 
+	/**
+	 * This is run to tear down a test calss after all test methods have been executed
+	 */
 	public static function tearDownAfterClass()
 	{
 		parent::tearDownAfterClass();
 	}
 
+	/**
+	 * Assert that multiple attribute/value pairs are set in the given
+	 *
+	 * @param   []StdClass $objects
+	 * @param   Array      $checks
+	 */
 	public function assertAttributesValueInArrayOfObjects(Array $objects, Array $checks)
 	{
 		foreach ($checks as $check)
@@ -127,6 +190,14 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 		}
 	}
 
+	/**
+	 * Assert that there is an attrabute/value pair in atleast one of the given objects
+	 *
+	 * @param   []StdClass $objects
+	 * @param   string     $attribute
+	 * @param   mixed      $value
+	 * @return  bool
+	 */
 	public function assertAttributeValueInArrayOfObjects(Array $objects, $attribute, $value)
 	{
 		foreach ($objects as $object)
